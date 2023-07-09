a991c472d23592b462a21d59b86839df5591aa1f
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-07-09 00:40:25 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:"red",
+        
+      }}>
+      <Text style={{
+       textDecorationColor: "white",
+       color:"white",
+       fontSize: 50
+        
+      }}>Hello World! 🎉</Text>
+      <Text>Buenas familiaaaaaaaaa</Text>
+      <Text>Soy lucassssssssss mamaaaaaa</Text>
+    </View>
+  );
+};
+
+export default App;
